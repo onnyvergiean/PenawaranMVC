b22ed7d1f11ca42cc9f51f46855d5f42451603c0
onny <onny.s@students.amikom.ac.id>
2020-12-03 20:54:29 +0700

    Add MainWindowController class

---
+++ Peran Bagian MVC/MainWindowController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace Peran_Bagian_MVC
+{
+    class MainWindowController
+    {
+        KeranjangBelanja keranjangBelanja;
+
+        public MainWindowController(KeranjangBelanja keranjangBelanja)
+        {
+            this.keranjangBelanja = keranjangBelanja;
+        }
+
+        public void addItem(Item item)
+        {
+            this.keranjangBelanja.addItem(item);
+        }
+
+        public List<Item> getSelectedItems()
+        {
+            return this.keranjangBelanja.getItems();
+        }
+
+        public void deleteSelectedItem(Item item)
+        {
+            this.keranjangBelanja.removeItem(item);
+        }
+    }
+}

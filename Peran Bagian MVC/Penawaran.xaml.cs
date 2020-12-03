3239c003f3b58a9ba6a5d1f54df7251dddf9b769
onny <onny.s@students.amikom.ac.id>
2020-12-03 20:27:54 +0700

    Add project files.

---
+++ Peran Bagian MVC/Penawaran.xaml.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Peran_Bagian_MVC
+{
+    /// <summary>
+    /// Interaction logic for Penawaran.xaml
+    /// </summary>
+    public partial class Penawaran : Window
+    {
+        public Penawaran()
+        {
+            InitializeComponent();
+        }
+
+        private void listPenawaran_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+
+        }
+    }
+}

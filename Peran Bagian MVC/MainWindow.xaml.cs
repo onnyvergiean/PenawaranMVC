cd7c3dd319108fb6c71c4b9acff99e40e598c7d3
onny <onny.s@students.amikom.ac.id>
2020-12-03 21:00:32 +0700

    Update Codingan MainWIndow

--- Peran Bagian MVC/MainWindow.xaml.cs
+++ Peran Bagian MVC/MainWindow.xaml.cs
@@ -18,21 +18,91 @@ namespace Peran_Bagian_MVC
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class MainWindow : Window,
+       OnPenawaranChangedListener,
+       OnPaymentChangedListener,
+       OnKeranjangBelanjaChangedListener
     {
+        MainWindowController controller;
+        Payment payment;
+
         public MainWindow()
         {
             InitializeComponent();
+
+            payment = new Payment(this);
+            payment.setBalance(500000);
+            payment.setDeliveryFee(15000);
+            payment.setPromo(5000);
+
+            KeranjangBelanja keranjangBelanja = new KeranjangBelanja(payment, this);
+
+            controller = new MainWindowController(keranjangBelanja);
+
+            listBoxPesanan.ItemsSource = controller.getSelectedItems();
+
+            initializeView();
+
+        }
+
+        private void initializeView()
+        {
+            labelSubtotal.Content = 0;
+            labelGrantTotal.Content = 0;
+            labelBalance.Content = payment.getBalance();
+            labelPromoFee.Content = -payment.getPromo();
+            labelDeliveryFee.Content = payment.getDeliveryFee();
+        }
+
+        public void onPenawaranSelected(Item item)
+        {
+            controller.addItem(item);
         }
 
         private void onButtonAddItemClicked(object sender, RoutedEventArgs e)
         {
-
+            Penawaran penawaranWindow = new Penawaran();
+            penawaranWindow.SetOnItemSelectedListener(this);
+            penawaranWindow.Show();
         }
 
         private void listBoxPesanan_ItemClicked(object sender, MouseButtonEventArgs e)
         {
+            if (MessageBox.Show("Kamu ingin menghapus item ini?",
+                    "Konfirmasi", MessageBoxButton.YesNo) == MessageBoxResult.Yes)
+            {
+                ListBox listBox = sender as ListBox;
+                Item item = listBox.SelectedItem as Item;
+                controller.deleteSelectedItem(item);
+            }
+        }
 
+
+        public void onSelectedPenawaranDeleted()
+        {
+            listBoxPesanan.Items.Refresh();
+        }
+
+        public void onSelectedPenawaranAdded()
+        {
+            listBoxPesanan.Items.Refresh();
+        }
+
+        public void onPriceUpdated(double subtotal, double grantTotal, double balance)
+        {
+            labelSubtotal.Content = subtotal;
+            labelBalance.Content = balance;
+            labelGrantTotal.Content = grantTotal;
+        }
+
+        public void removeItemSucceed()
+        {
+            listBoxPesanan.Items.Refresh();
+        }
+
+        public void addItemSucceed()
+        {
+            listBoxPesanan.Items.Refresh();
         }
     }
 }

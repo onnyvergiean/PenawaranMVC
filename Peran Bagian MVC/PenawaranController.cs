bc1f346e2d1aeb12147a2c7a7890c00b3d32a833
onny <onny.s@students.amikom.ac.id>
2020-12-03 20:49:03 +0700

    Add PenawaranController.cs

---
+++ Peran Bagian MVC/PenawaranController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows.Media;
+
+namespace Peran_Bagian_MVC
+{
+    class PenawaranController
+    {
+        private List<Item> items;
+
+        public PenawaranController()
+        {
+            items = new List<Item>();
+        }
+
+        public void addItem(Item item)
+        {
+            this.items.Add(item);
+        }
+
+        public List<Item> GetItems()
+        {
+            return this.items;
+        }
+    }
+}
